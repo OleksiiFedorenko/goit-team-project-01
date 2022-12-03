07d21a09fed963b7ba2147f2dc07935ddac60370
Oleksii Fedorenko <fedorenkolesha@gmail.com>
2022-12-03 18:39:02 +0200

    Create slider for hotel page

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -41,3 +41,22 @@ new Swiper('.three-img-slider', {
         }
     },
 });
+
+new Swiper('.hotel-slider', {
+    navigation: {
+        nextEl: '.arrow-right',
+        prevEl: '.arrow-left'
+    },
+    
+    slidesPerView: "auto",
+
+    spaceBetween: 16,
+
+    loop: true,
+
+    breakpoints: {
+        1024: {
+            spaceBetween: 40,
+        }
+    },
+});

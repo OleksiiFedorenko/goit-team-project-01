b5467e71ccf252f796ff2116030595844a03a8f2
Ivanna Syniebok <zolota305@gmail.com>
2022-12-04 21:06:06 +0200

    added modal-room
    додала модальне вікно для забронювати номер

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './js/hero-slider';
 import "./js/swiper.js";
+import "./js/select-date.js";
+import "./js/select-exit.js";
+import "./js/select-night.js";
+import "./js/select-room.js";
+import "./js/select-table.js";
+import "./js/modal.js";

6e65081450421be596fd25b5f1983b04281543d9
Tetiana Ruzhetska <tetiana.ruzhetska@gmail.com>
2022-12-07 02:02:42 +0200

    Add scroll for anchore link to page Restaurant

--- src/js/anchor-scroll.js
+++ src/js/anchor-scroll.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 
 $(document).ready(function () {
   // Add smooth scrolling to all links
-  $('a').on('click', function (event) {
+  $('a.hero__main-button').on('click', function (event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== '') {
       // Prevent default anchor click behavior
